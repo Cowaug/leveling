60b0bf9ee04fb99dea65205c34eb9023f4c17230
Cowaug <exos2k@gmail.com>
2025-11-09 15:48:12 +0700

    feat: init commit

---
+++ src/client/java/cowaug/leveling/LevelingClient.java
@@ -0,0 +1,10 @@
+package cowaug.leveling;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LevelingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
